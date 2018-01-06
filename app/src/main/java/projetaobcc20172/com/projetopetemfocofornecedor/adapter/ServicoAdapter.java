e1da41b2b0eca5293b82cd127ffb35d26882c92c
Felipe Oliveira <flpdias14@gmail.com>
2018-01-06 16:14:11 -0300

    [#152261851, #152261913] Correção layout

--- app/src/main/java/projetaobcc20172/com/projetopetemfocofornecedor/adapter/ServicoAdapter.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfocofornecedor/adapter/ServicoAdapter.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
