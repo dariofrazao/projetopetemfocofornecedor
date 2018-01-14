d36d25f03e4aaefece129a7196dca4ec74f92000
Raul Sousa <raulpedrouag@gmail.com>
2018-01-14 18:14:18 -0300

    add correções do codacy

--- app/src/main/java/projetaobcc20172/com/projetopetemfocofornecedor/activity/MainActivity.java
+++ app/src/main/java/projetaobcc20172/com/projetopetemfocofornecedor/activity/MainActivity.java
@@ -16,7 +16,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import projetaobcc20172.com.projetopetemfocofornecedor.R;
